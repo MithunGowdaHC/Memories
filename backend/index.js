83c62704bd8c018078ca51e3177e2d84f4aa0c33
MithunGowda <mithungowdahc12a@gmail.com>
2024-12-27 09:44:40 +0530

    ready now

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,12 @@ const connectDB = async () => {
 dotenv.config()
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname,"/images")))
-app.use(cors({origin : "https://memories-frontend-78cz.onrender.com/" , credentials:true}))
+// app.use(cors({origin : "https://memories-frontend-78cz.onrender.com" , credentials:true}))
+app.use(cors({
+  origin: ["https://memories-frontend-78cz.onrender.com", "http://localhost:5173"],
+  credentials: true,
+}));
+
 app.use(cookieParser())
 app.use("/api/auth", authRoute)
 app.use("/api/users", usersRoute)
