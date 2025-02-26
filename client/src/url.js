77fe2101870f97510be5bf4d978222bfcdc45011
MithunGowda <mithungowdahc12a@gmail.com>
2025-02-26 22:52:32 +0530

    feb 26

--- client/src/url.js
+++ client/src/url.js
@@ -1,4 +1,4 @@
-// export const URL = "http://localhost:5000"
-// export const IF = "http://localhost:5000/images/"
-export const URL = "https://memories-37nd.onrender.com"
-export const IF = "https://memories-37nd.onrender.com/images/"
+export const URL = "http://localhost:5000"
+export const IF = "http://localhost:5000/images/"
+// export const URL = "https://memories-37nd.onrender.com"
+// export const IF = "https://memories-37nd.onrender.com/images/"
